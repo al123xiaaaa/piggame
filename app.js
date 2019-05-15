e78a712145d209b0e86b50de2bf9fe4b9ed99ab5
Guangwei Xia <al123xiaaaa@gmail.com>
2019-05-15 15:13:59 +0800

    two dices branch

--- app.js
+++ app.js
@@ -18,16 +18,21 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         return;
     }
 
-    var dice = Math.floor(Math.random() * 6) + 1;
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+    var diceDOM1 = document.getElementById('dice1');
+    var diceDOM2 = document.getElementById('dice2');
+    diceDOM1.style.display = 'block';
+    diceDOM2.style.display = 'block';
+    diceDOM1.src = 'dice-' + dice1 + '.png';
+    diceDOM2.src = 'dice-' + dice2 + '.png';
+    console.log('dice1 = ' + dice1 + ',dice2 = ' + dice2);
 
-    if (dice !== 1) {
-        roundScore += dice;
-        document.getElementById('current-' + activePlayer).textContent = roundScore;
-    } else {
+    if (dice1 === 1 && dice2 === 1) {
         nextPlayer();
+    } else {
+        roundScore += dice1 + dice2;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
     }
 });
 
@@ -37,8 +42,9 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     }
     scores[activePlayer] += roundScore;
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    hideDices();
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
         document.getElementById('name-' + activePlayer).textContent = 'Winner!'
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -52,6 +58,11 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
+function hideDices() {
+    document.getElementById('dice1').style.display = 'none';
+    document.getElementById('dice2').style.display = 'none';
+}
+
 function init() {
     scores = [0, 0];
     roundScore = 0;
@@ -63,7 +74,7 @@ function init() {
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-1').textContent = '0';
 
-    document.querySelector('.dice').style.display = 'none';
+    hideDices();
     document.getElementById('name-0').textContent = 'Player 1'
     document.getElementById('name-1').textContent = 'Player 2'
 
@@ -81,5 +92,5 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    document.querySelector('.dice').style.display = 'none';
+    hideDices();
 }
